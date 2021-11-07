243a82db4c4dfe76e1e738805ea6a580d303fc5f
shadowdune <shadowxdune@gmail.com>
2021-11-07 05:33:12 +0530

    elixir

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xdroid_vayu.mk
+    $(LOCAL_DIR)/aosp_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    xdroid_vayu-eng \
-    xdroid_vayu-userdebug \
-    xdroid_vayu-user
+    aosp_vayu-eng \
+    aosp_vayu-userdebug \
+    aosp_vayu-user
--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -10,15 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
-$(call inherit-product, device/xiaomi/vayu/device-hidl.mk)
 
-# Inherit some common xd stuff.
-$(call inherit-product, vendor/xdroid/config/common.mk)
-XDROID_BOOT_DARK := true
+# Inherit common Aosp configurations
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := xdroid_vayu
+PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
